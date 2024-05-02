413d22e9162cb419920688886faec1d6d5e4529a
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-02 18:25:35 -0300

    SearchBar y empezamos con la creación de las cards

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,12 +1,32 @@
-import Navbar from "../../components/Navbar/Navbar"
-
+import NoteCard from "../../components/Cards/NoteCard";
+import Navbar from "../../components/Navbar/Navbar";
+import { MdAdd } from 'react-icons/md'
 
 const Home = () => {
-    return(
-        <>
-            <Navbar />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
 
-export default Home
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meting on 7th April"
+            date="3rd Apr 2024"
+            content="Meting on 7th April"
+            tags="#Metting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <MdAdd className="text-[32] text-white" />
+      </button>
+    </>
+  );
+};
+
+export default Home;
