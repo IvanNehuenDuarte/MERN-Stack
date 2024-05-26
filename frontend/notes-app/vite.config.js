b7bc37ac65a12d48c4b425d8f7a48297411044c0
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-25 23:14:04 -0300

    config vite

--- frontend/notes-app/vite.config.js
+++ frontend/notes-app/vite.config.js
@@ -1,19 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
   },
   server: {
-    proxy: {
-      "/": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    open: true,
   },
 });
