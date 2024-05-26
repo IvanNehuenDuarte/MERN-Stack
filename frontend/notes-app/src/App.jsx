37e53a831686690a57264352cfbfc6816adced8e
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-25 22:51:32 -0300

    redirect

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 
-
 const routes = (
   <Router>
     <Routes>
+      <Route exact path="/">
+        <Redirect to="/dashboard" />
+      </Route>
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<SignUp />} />
